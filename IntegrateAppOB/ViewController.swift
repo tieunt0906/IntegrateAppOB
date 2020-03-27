c98f85b702a762a1f8203af9a5d6f2617dcd2f9b
tieunt0906 <tieunt.dev97@gmail.com>
2020-03-27 08:48:56 +0700

    Initial Commit

---
+++ IntegrateAppOB/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IntegrateAppOB
+//
+//  Created by vbee on 3/27/20.
+//  Copyright © 2020 vbee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
